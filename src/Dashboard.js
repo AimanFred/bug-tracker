6d38d44b0b2131293aa2d13ef5a6e8176c346b5e
AimanFred <irzanfarish@gmail.com>
2022-08-17 11:28:23 +0800

    add Click button and event handler

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,18 @@
 const Dashboard = () => {
-    return (
-        <div className="dashboard">
-            <h2>Dashboard</h2>
-        </div>
-    );
-}
- 
+  const handleClick = (e) => {
+    console.log("Hello, User", e);
+  };
+  const handleClickAgain = (name, e) => {
+    console.log("Hello, ", name, e);
+  };
+
+  return (
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <button onClick={ handleClick }>Click Here</button>
+      <button onClick={ (e) => handleClickAgain("Mario", e) }>Click Here Again</button>
+    </div>
+  );
+};
+
 export default Dashboard;
