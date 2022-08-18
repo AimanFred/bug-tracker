2421f24a5e6d0746ebaac280ba878285a6907220
AimanFred <irzanfarish@gmail.com>
2022-08-19 07:52:30 +0800

    add delete function to IssueDetails component with DELETE request to the server

--- src/IssueDetails.js
+++ src/IssueDetails.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const IssueDetails = () => {
   const { id } = useParams();
   const { data: issue, error, isLoading } = useFetch("http://localhost:8000/issues/" + id);
+  const history = useHistory();
+
+  const handleDelete = () => {
+    fetch("http://localhost:8000/issues/" + issue.id, {
+      method: "DELETE"
+    }).then(() => {
+      history.push("/")
+    })
+  }
 
   return ( 
     <div className="issue-details">
@@ -14,6 +24,7 @@ const IssueDetails = () => {
           <h2>{ issue.title }</h2>
           <p>Issued by { issue.author }</p>
           <div>{ issue.body }</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
