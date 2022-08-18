3834a5cfd1fc0f9ddd0140d62179616b0400e06f
AimanFred <irzanfarish@gmail.com>
2022-08-18 09:32:28 +0800

    change <a> tag to <Link> tag for react routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Issue Tracker</h1>
             <div className="links">
-                <a href="/">Dashboard</a>
-                <a href="/create">New Issue</a>
+                <Link to="/">Dashboard</Link>
+                <Link to="/create">New Issue</Link>
             </div>
         </nav>
     );
